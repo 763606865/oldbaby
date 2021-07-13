88f023ae9564e024ef72972d7903e7aef19ae530
刘骏霖 <junlin.liu@holla.world>
2021-07-13 15:03:20 +0800

    admin构建

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\IndexController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/', [HomeController::class, 'index']);
+Route::post('/', [IndexController::class, 'index']);

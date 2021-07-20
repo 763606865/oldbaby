1fbcf4a004c98e8ca522b25e86ad8d8466f4b93d
刘骏霖 <junlin.liu@holla.world>
2021-07-20 16:25:17 +0800

    取消默认值

--- config/filesystems.php
+++ config/filesystems.php
@@ -71,8 +71,8 @@ return [
 
         'oss' => [
             'driver'        => 'oss',
-            'access_id'     => env('ALIYUN_ACCESS_KEY','LTAI5t8in8HQzAsTytk2zq3m'),
-            'access_key'    => env('ALIYUN_ACCESS_SECRET','NeHXLRgZaARkguNhvgA2K38kduJ45Y'),
+            'access_id'     => env('ALIYUN_ACCESS_KEY',''),
+            'access_key'    => env('ALIYUN_ACCESS_SECRET',''),
             'bucket'        => env('ALIYUN_BUCKET','oldbaby'),
             'endpoint'      => env('ALIYUN_WEB_POINT','oss-cn-beijing.aliyuncs.com'), // OSS 外网节点或自定义外部域名
             //'endpoint_internal' => '<internal endpoint [OSS内网节点] 如：oss-cn-shenzhen-internal.aliyuncs.com>', // v2.0.4 新增配置属性，如果为空，则默认使用 endpoint 配置(由于内网上传有点小问题未解决，请大家暂时不要使用内网节点上传，正在与阿里技术沟通中)
